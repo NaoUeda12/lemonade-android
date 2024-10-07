e71abee9cc146d05c682b72b2d6fe1155772a5ec
NaoUeda12 <ueda.nao@chrono.co.jp>
2024-10-07 14:46:46 +0900

    テストコミット

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -67,7 +67,7 @@ fun LemonApp() {
                         fontWeight = FontWeight.Bold
                     )
                 },
-               
+
             )
         }
     ) { innerPadding ->
